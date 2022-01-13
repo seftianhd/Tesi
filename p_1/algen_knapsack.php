12e28a349105a78d58520d1ff33af4c37d6c5b20
seftianhd <48037525+seftianhd@users.noreply.github.com>
2022-01-13 20:39:45 +0700

    update pertemuan

--- p_1/algen_knapsack.php
+++ p_1/algen_knapsack.php
@@ -1,70 +1,61 @@
 <?php
 
-class catalogue{
-    function createProductColum($columns, $listofRawProduct){
-        foreach(array_keys($listofRawProduct) as $listofRawProductKey){
-            $listofRawProduct[$columns[$listofRawProductKey]]= $listofRawProduct[$listofRawProductKey];
-            unset($listofRawProduct[$listofRawProductKey]);
+class Catalogue{
+    function createProductColumn($columns, $listOfRawProduct){
+        foreach (array_keys($listOfRawProduct) as $listOfRawProductKey) {
+            $listOfRawProduct[] = $listOfRawProduct[$listOfRawProductKey];
+            unset($listOfRawProduct[$listOfRawProductKey]);
         }
-        return $listofRawProduct;
-
+        return $listOfRawProduct;
     }
 
+    function product($parameters){
+        $collectionOfListProduct = [];
 
-    function products($parameter){
-        $koleksiListProdak = [];
+        $raw_data = file($parameters['file_name']);
+        foreach ($raw_data as $listOfRawProduct) {
+            $collectionOfListProduct[] = $this->createProductColumn($parameters['columns'], explode(",", $listOfRawProduct));
+        }
 
-       $raw_data= file($parameter['file_name']);
-       foreach ($raw_data as $listofRawProduct){
-           $koleksiListProdak[]= $this->createProductColum($parameter['columns'], explode(",",$listofRawProduct));
-       }
-
-       return [    
-        'product' => $koleksiListProdak,
-        'gen_length' => count($koleksiListProdak)
-    ];
-
+        return [
+            'product' => $collectionOfListProduct,
+            'gen_length' => count($collectionOfListProduct)
+        ];
     }
 }
-
 
 
 class PopulationGenerator{
-    function createIndividu($parameter){
-        $catlogue = new catalogue;
-        $lengthgen = $catlogue->products($parameter)['gen_length'];
-        for ($i = 0; $i <= $lengthgen-1; $i++){
-            $ret[] = rand(0,1);
+    
+    function createIndividu($parameters){
+        $catalogue = new Catalogue;
+        $lengthOfGen = $catalogue->product($parameters)['gen_length'];
+        for ($i = 0; $i <= $lengthOfGen - 1; $i++) {
+            $ret[] = rand(0, 1);
         }
 
-            return $ret;
+        return $ret;
     }
 
-    function createPopulation($parameter){
-        for ($i = 0; $i <= $parameter['population_size']; $i++){
-           $ret[] = $this->createIndividu($parameter);
+    function createPopulation($parameters){
+        for ($i = 0; $i <= $parameters['population_size']; $i++) {
+            $ret[] = $this->createIndividu($parameters);
         }
 
-        foreach ($ret as $key => $val){
+        foreach ($ret as $key => $val) {
             print_r($val);
-            echo '<br>';
         }
     }
-
 }
 
-$parameter = [
 
-    'file_name' => 'products.txt', 
-    'columns'   => ['item', 'price'], 
+$parameters = [
+    'file_name' => 'products.txt',
+    'columns' => ['item', 'price'],
     'population_size' => 10
 ];
 
-$ktalog = new catalogue;
-$ktalog->products($parameter);
+$katalog = new Catalogue;
 
-$initalPopulation = new PopulationGenerator;
-$initalPopulation->createPopulation($parameter);
-
-
-?>
+$initialPopulation = new PopulationGenerator;
+$initialPopulation->createPopulation($parameters);
